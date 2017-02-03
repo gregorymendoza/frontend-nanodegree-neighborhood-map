7ff0bc0f5c5c0c3af1ab7ae1ea1465ac014ea1b2
Gregory Mendoza <gregmendgreg@gmail.com>
2017-02-02 21:37:01 -0400

    Add basic map functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,116 @@
-app.js
+// Create a map variable
+var map;
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+
+// Function to initialize the map within the map div
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 10.480594, lng: -66.903606},
+		zoom: 20
+	});
+
+	var locations = [
+        {title: 'Sambil', location: {lat: 10.489459, lng: -66.854343}},
+        {title: 'El Recreo', location: {lat: 10.491714, lng: -66.877137}},
+        {title: 'El Tolón', location: {lat:  10.480487, lng: -66.860553}},
+        {title: 'San Ignacio', location: {lat: 10.4978, lng: -66.8565}},
+        {title: 'Paseo El Hatillo', location: {lat: 10.423652, lng: -66.824018}},
+        {title: 'Plaza las Américas', location: {lat: 10.458384, lng: -66.828957}}
+    ];
+
+    var bounds = window.mapBounds;
+
+    var largeInfowindow = new google.maps.InfoWindow();
+
+    // Style the markers a bit. This will be our listing marker icon.
+	var defaultIcon = makeMarkerIcon('0091ff');
+
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('ffff24');
+
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+		// Get the position from the location array.
+		var position = locations[i].location;
+		var title = locations[i].title;
+		// Create a marker per location, and put into markers array.
+		var marker = new google.maps.Marker({
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			icon: defaultIcon,
+			id: i
+		});
+		// Push the marker to our array of markers.
+		markers.push(marker);
+		// Create an onclick event to open the large infowindow at each marker.
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfowindow);
+		});
+		// Two event listeners - one for mouseover, one for mouseout,
+		// to change the colors back and forth.
+		marker.addListener('mouseover', function() {
+			this.setIcon(highlightedIcon);
+		});
+		marker.addListener('mouseout', function() {
+			this.setIcon(defaultIcon);
+		});
+    }
+
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+		bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+
+    map.setCenter(bounds.getCenter());
+
+    // Sets the boundaries of the map based on pin locations
+    window.mapBounds = bounds;
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		// Clear the infowindow content to give the streetview time to load.
+		infowindow.setContent('');
+		infowindow.marker = marker;
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		});
+
+		infowindow.setContent('<div>' + marker.title + '</div>');
+
+		infowindow.open(map, marker);
+	}
+}
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+	var markerImage = new google.maps.MarkerImage(
+		'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+		'|40|_|%E2%80%A2',
+		new google.maps.Size(21, 34),
+		new google.maps.Point(0, 0),
+		new google.maps.Point(10, 34),
+		new google.maps.Size(21,34));
+	return markerImage;
+}
+
+// Vanilla JS way to listen for resizing of the window
+// and adjust map bounds
+window.addEventListener('resize', function(e) {
+    //Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
+});
